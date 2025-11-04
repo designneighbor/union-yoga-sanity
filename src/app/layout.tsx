34f032d99685291789c22cc2d852783b0fcc2efe
David <hello@david-lewis.co>
2025-11-03 21:49:48 -0500

    added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,17 @@
 import "@/app/globals.css";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Union Yoga | Black-Owned Yoga Studio in Baltimore",
+  description: "Union Yoga is a yoga studio in Baltimore, Maryland. It is a place to practice yoga, meditate, and connect with others.",
+  keywords: ["yoga", "meditation", "baltimore", "union", "yoga", "union yoga", "maryland"],
+  authors: [{ name: "Union Yoga" }],
+  robots: "index, follow",
+  icons: {
+    icon: "/site_icon.png",
+    apple: "/site_icon.png",
+  },
+};
 
 export default function RootLayout({
   children,
@@ -7,15 +20,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-
-      <title>Union Yoga | Black-Owned Yoga Studio in Baltimore</title>
-      <meta name="description" content="Union Yoga is a yoga studio in Baltimore, Maryland. It is a place to practice yoga, meditate, and connect with others." />
-      <meta name="keywords" content="yoga, meditation, baltimore, union, yoga, union yoga, maryland" />
-      <meta name="author" content="Union Yoga" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta name="robots" content="index, follow" />
-      <meta name="googlebot" content="index, follow" />
-      <meta name="bingbot" content="index, follow" />
 
       <body className="font-sans">{children}</body>
 
